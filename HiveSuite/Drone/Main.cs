2f6fc469f87d727a5d81a4b358738ab411243984
Aaron Van Prooyen <aaron.vanprooyen@ostusa.com>
2016-10-21 12:21:39 -0400

    yay things and stuff

---
+++ HiveSuite/Drone/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiveSuite.Drone
+{
+    /// <summary>
+    /// Main loop
+    /// </summary>
+    class Main
+    {
+        // TODO: Communications - Layer
+        // TODO: Talk to Hive - task
+        // TODO: Init Binary Cache - task / layer
+        // TODO: Execute Task - 
+        // TODO: Cleanup - task
+        // TODO: Close Comm - task
+    }
+}
