a5e69a1e2facabbb651b6e0c5de8ccd5c07d9c56
Aaron Van Prooyen <aaron.vanprooyen@ostusa.com>
2016-11-21 09:16:13 -0500

    The start of the queen

---
+++ HiveSuite/Queen/Queen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiveSuite.Queen
+{
+    public class Queen
+    {
+        public static void MainLoop()
+        {
+
+        }
+    }
+}
