a637ba56a313130cec183c86de40a27458d8afc3
Aaron Van Prooyen <aaron.vanprooyen@ostusa.com>
2016-12-12 15:06:24 -0500

    one more change..... now i'm done with the build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,13 @@
 $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 $a = "HiveSuite\HiveSuite.sln /target:Clean /target:Build"
 $nugetrestore = "nuget restore .\HiveSuite\HiveSuite.sln"
+
+if(![System.IO.File]::Exists($msbuild))
+{
+    Write-Host "You must have visual studio 2015 or better to build this project." -ForegroundColor Red 
+    exit   
+}
+
 Invoke-Expression "$nugetrestore"
 Invoke-Expression "$msbuild $a"
 Write-Host "If you are having issues building the project from this scrpit, please run prereqs.ps1." -ForegroundColor Red
