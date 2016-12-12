2513480ec6fd1f6a25947815028edff31371c328
Aaron Van Prooyen <aaron.vanprooyen@ostusa.com>
2016-12-12 14:52:00 -0500

    fixed some text and colors in the build script

--- build.ps1
+++ build.ps1
@@ -3,3 +3,5 @@ $a = "HiveSuite\HiveSuite.sln /target:Clean /target:Build"
 $nugetrestore = "nuget restore .\HiveSuite\HiveSuite.sln"
 Invoke-Expression "$nugetrestore"
 Invoke-Expression "$msbuild $a"
+Write-Host "If you are having issues building the project from this scrpit, please run prereqs.ps1." -ForegroundColor Red
+Write-Host "If this project can be built on Linux than there will be a build.sh script." -ForegroundColor Green
